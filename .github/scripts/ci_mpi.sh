0d3beb4bd210dbd52507996c4f6fb5045f38f964
rhodrin <rhodri.nelson@gmail.com>
2020-09-01 16:21:34 +0100

    .

--- .github/scripts/ci_mpi.sh
+++ .github/scripts/ci_mpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load gcc/9.2.0
 module load openmpi/4.0.5-mlnx-gcc
-source ~/d/sw/miniconda3/4.8.2/etc/profile.d/conda.sh
+source /d/sw/miniconda3/4.8.2/etc/profile.d/conda.sh
 conda activate devito
 pytest tests/test_subdomains.py
