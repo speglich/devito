10aef84cc4d6bac0f80ca26bd5f2ad14ea5efda4
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-11-23 13:00:25 +0100

    Temporary work around for clang11 offloading

--- devito/ir/iet/visitors.py
+++ devito/ir/iet/visitors.py
@@ -211,18 +211,14 @@ class CGen(Visitor):
                                                 f._C_field_data)
             else:
                 rvalue = '(%s (*)%s) %s' % (f._C_typedata, shape, f._C_name)
-            lvalue = c.AlignedAttribute(f._data_alignment,
-                                        c.Value(f._C_typedata,
-                                                '(*restrict %s)%s' % (f.name, shape)))
+            lvalue = c.Value(f._C_typedata, '(*restrict %s)%s' % (f.name, shape))
         return c.Initializer(lvalue, rvalue)
 
     def visit_Dereference(self, o):
         a0, a1 = o.functions
         shape = ''.join("[%s]" % ccode(i) for i in a0.symbolic_shape[1:])
         rvalue = '(%s (*)%s) %s[%s]' % (a1._C_typedata, shape, a1.name, a1.dim.name)
-        lvalue = c.AlignedAttribute(a0._data_alignment,
-                                    c.Value(a0._C_typedata,
-                                            '(*restrict %s)%s' % (a0.name, shape)))
+        lvalue = c.Value(a0._C_typedata, '(*restrict %s)%s' % (a0.name, shape))
         return c.Initializer(lvalue, rvalue)
 
     def visit_tuple(self, o):
